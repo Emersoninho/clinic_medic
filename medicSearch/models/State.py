458784801bfff6f41d39452d29ee55f8be3e75bc
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-18 21:27:22 -0300

    migrate
    arquivo de migrações

--- medicSearch/models/State.py
+++ medicSearch/models/State.py
@@ -2,7 +2,7 @@ from medicSearch.models import *
 
 class State(models.Model):
     name = models.CharField(null=False, max_length=20)
-    state = models.BooleanField(default=True)
+    status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
