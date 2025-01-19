4e1ffdaadb41365f5243bf0d293308262fd82980
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-19 00:13:59 -0300

    urls
    urls principal

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -22,4 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('medicSearch.urls.HomeUrls')),
+    path('profile/', include('medicSearch.urls.ProfileUrls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
